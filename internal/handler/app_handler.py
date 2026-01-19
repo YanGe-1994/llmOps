c6d1a9007bd2ae5b6f5b12356353fd96b681c6ab
YanGe-1994 <yange19940310@gmail.com>
2026-01-19 17:18:54 +0800

    feat: langchain基础用法

--- internal/handler/app_handler.py
+++ internal/handler/app_handler.py
@@ -9,14 +9,24 @@ import os
 import uuid
 
 from flask import request
-from openai import OpenAI
+from langchain_core.prompts import ChatPromptTemplate
 from injector import inject
 from dataclasses import dataclass
+
+from openai import responses
 
 from internal.schema.app_schema import CompletionReq
 from internal.service import AppService
 from pkg.response import success_json, validate_error_json, success_message
 from internal.exception import FailException
+
+from pydantic import BaseModel,Field
+from langchain_community.llms import Tongyi
+from langchain_core.output_parsers import JsonOutputParser
+
+class Joke(BaseModel):
+    joke: str = Field(description="回答用户的冷笑话")
+    punchline: str= Field(description="冷笑话的笑点")
 
 @inject
 @dataclass
@@ -30,16 +40,34 @@ class AppHandler:
             return validate_error_json(req.errors)
 
         query = request.json.get('query')
-        # 2. 构建openid客户端
-        client = OpenAI(
-            api_key=os.environ.get("OPEN_API_KEY"),
-            base_url=os.environ.get("BASE_URL"),
-        )
-        completion = client.chat.completions.create(
+        # 生成提示词
+        prompt = ChatPromptTemplate.from_template("""
+            你是一个超级搞笑的脱口秀演员，请认真回答用户的问题。
+            {format_instructions}
+            用户问题: {query}
+        """)
+
+        # 构建通义千问llm客户端
+        llm = Tongyi(
             model="qwen-plus",
-            messages=[{'role': 'user', 'content': query}]
+            temperature=0.2,
+            responses_format=Joke,
         )
-        content = completion.choices[0].message.content
+
+        # 构建输出解析器
+        # parser = StrOutputParser()
+
+        parser = JsonOutputParser(pydantic_object=Joke)
+        # 构建链式调用
+        chain = prompt | llm | parser
+
+        # 执行链式调用
+        content = chain.invoke({
+            "query": query,
+            "format_instructions": parser.get_format_instructions()
+        })
+
+        # 返回结果
         return success_json({'content': content})
 
     def ping(self):
